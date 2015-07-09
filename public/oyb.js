ae6e60cee1ae969a0165cac6f4512e1271283431
J.D. Warren <johndavid400@gmail.com>
2015-07-09 15:16:08 -0500

    update

--- public/oyb.js
+++ public/oyb.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
   $('body').on("click", ".update", function(event){
     event.preventDefault();
     $(".oyb").html("Loading...");
-    $.get("http://localhost:3000/get_passages?day=" + $(this).attr("data-id"), function(data){ $(".oyb").html(data); });
+    $.get("http://oyb.herokuapp.com/get_passages?day=" + $(this).attr("data-id"), function(data){ $(".oyb").html(data); });
   });
 });
 
 // the function that loads the data from the server
 function loadScripture(divClass) {
-  $.get("http://localhost:3000/get_passages", function(data){ $(".oyb").html(data); });
+  $.get("http://oyb.herokuapp.com/get_passages", function(data){ $(".oyb").html(data); });
 }
 
