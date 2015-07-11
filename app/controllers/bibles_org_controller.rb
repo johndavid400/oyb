7d47f9c3353997deef3288f4d706103eb53b47b3
J.D. Warren <johndavid400@gmail.com>
2015-07-11 16:00:47 -0500

    testing some optionsg

--- app/controllers/bibles_org_controller.rb
+++ app/controllers/bibles_org_controller.rb
@@ -10,27 +10,38 @@ class BiblesOrgController < ApplicationController
   def get_passages
     @result = []
     @day = params[:day].present? ? Day.find(params[:day]) : Day.find(Date.today.yday())
-    version = "eng-KJV" # test with KJV english
-    if RedisConnection.new.connection.get("oyb#{@day.id}v#{version}")
-      @text = RedisConnection.new.connection.get("oyb#{@day.id}v#{version}")
-    else
-      insert_header
-      passages = @day.passages
-      @result.push(@day.formatted_devotional)
-      passages.each do |p|
-        passage = p.gsub(/\s/, "+")
-        get("passages.js?q[]=#{passage}&version=#{version}")
-        @result.push("<hr>").push("<h2>#{p}</h2>")
-        @result.push(@resp["response"]["search"]["result"]["passages"].map{|s| s["text"] }.join("<p>"))
-      end
-      @text = @result.join("<p>")
-      RedisConnection.new.connection.set("oyb#{@day.id}v#{version}", @text)
-    end
+    @version = "eng-KJV" # test with KJV english
+    build_header
+    build_devotional if params[:devotional] && params[:devotional].to_s == "true"
+    build_verses
+    @text = @result.join("<p>")
     render :json => @text.to_json
   end
 
-  def insert_header
+  def build_header
     @result.push("<h1>One Year Bible - <span class='date'>#{day_to_date(@day.id)}</span></h1><hr><span class='oyb-nav-links'><a href='#' class='oyb-prev update' data-id='#{@day.id - 1}'>Prev</a><a href='#' class='oyb-next update' data-id='#{@day.id + 1}'>Next</a></span>")
+  end
+
+  def build_devotional
+    @result.push(@day.formatted_devotional).push("<hr>")
+  end
+
+  def build_verses
+    if RedisConnection.new.connection.get("oyb#{@day.id}v#{@version}") && params[:cache]
+      @result.push(RedisConnection.new.connection.get("oyb#{@day.id}v#{@version}"))
+    else
+      content = []
+      passages = @day.passages
+      passages.each do |p|
+        passage = p.gsub(/\s/, "+")
+        get("passages.js?q[]=#{passage}&version=#{@version}")
+        content.push("<h2>#{p}</h2>")
+        content.push(@resp["response"]["search"]["result"]["passages"].map{|s| s["text"] }.join("<p>"))
+      end
+      bible_content = content.join("<p>")
+      RedisConnection.new.connection.set("oyb#{@day.id}v#{@version}", bible_content)
+      @result.push(bible_content)
+    end
   end
 
   def get(endpoint)
--- public/oyb.js
+++ public/oyb.js
@@ -4,12 +4,13 @@ $(document).ready(function(){
   $('body').on("click", ".update", function(event){
     event.preventDefault();
     $(".oyb").html("Loading...");
-    $.get("http://oyb.herokuapp.com/get_passages?day=" + $(this).attr("data-id"), function(data){ $(".oyb").html(data); });
+    // $.get("http://oyb.herokuapp.com/get_passages?day=" + $(this).attr("data-id"), function(data){ $(".oyb").html(data); });
+    $.get("http://localhost:3000/get_passages?cache=true&devotional=false&day=" + $(this).attr("data-id"), function(data){ $(".oyb").html(data); });
   });
 });
 
 // the function that loads the data from the server
 function loadScripture() {
-  $.get("http://oyb.herokuapp.com/get_passages", function(data){ $(".oyb").html(data); });
+  $.get("http://oyb.herokuapp.com/get_passages?cache=true&devotional=false", function(data){ $(".oyb").html(data); });
 }
 
