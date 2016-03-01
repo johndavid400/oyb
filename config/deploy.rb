16b4d6c376ae39df66da9eb622dca3e17c2ecf45
J.D. Warren <johndavid400@gmail.com>
2016-03-01 13:41:57 -0600

    update ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,11 +26,11 @@ set :log_level, :debug
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 #set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 #set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', "public/assets")
 
 # Default value for default_env is {}
