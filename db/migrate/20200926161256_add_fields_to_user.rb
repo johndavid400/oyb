1cc6d7cfc2c1733e97af624b3e59d1690e53f67a
johndavid400 <johndavid400@gmail.com>
2020-09-26 11:18:09 -0500

    Add api key for user

---
+++ db/migrate/20200926161256_add_fields_to_user.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :token, :string
+    add_index :users, :token, unique: true
+    add_column :users, :api_bible_key, :string
+    add_index :users, :api_bible_key, unique: true
+    add_column :users, :config, :jsonb
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_160757) do
+ActiveRecord::Schema.define(version: 2020_09_26_161256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,9 +47,14 @@ ActiveRecord::Schema.define(version: 2020_09_26_160757) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
+    t.string "api_bible_key"
+    t.jsonb "config"
+    t.index ["api_bible_key"], name: "index_users_on_api_bible_key", unique: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["token"], name: "index_users_on_token", unique: true
   end
 
 end
